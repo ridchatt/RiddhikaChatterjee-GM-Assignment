e1321f048454c55a1675a798b4bba9999e0db36b
ridchatt <riddhika.chatterjee0005@gmail.com>
2024-08-08 23:25:09 +0200

    modified hover function

--- e2e/ui/pages/tooltip-page.ts
+++ e2e/ui/pages/tooltip-page.ts
@@ -9,12 +9,21 @@ export class TooltipPage {
     this.tooltip = this.page.locator(".tooltip-inner");
   }
 
-   // Hover over the button and ensure the tooltip becomes visible
+  // Hover over the button and ensure the tooltip becomes visible
   async hoverOverButton() {
     await this.page.hover("#toolTipButton");
-    await this.page.waitForSelector(".tooltip-inner", {
-      state: "visible",
-      timeout: 5000,
-    });
+
+    for (let i = 0; i < 3; i++) {
+      try {
+        await this.page.waitForSelector(".tooltip-inner", {
+          state: "visible",
+          timeout: 7000,
+        });
+        break;
+      } catch (error) {
+        if (i === 2) throw error;
+        console.warn("Tooltip not found, retrying...");
+      }
+    }
   }
 }
